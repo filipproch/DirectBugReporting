832a1cce55e55861334ad9a9426fd61f26dab748
jacktech24 <filip@jacktech.cz>
2014-07-20 16:19:41 +0200

    Initial commit

---
+++ library/src/main/java/cz/jacktech/dbr/library/ReportingService.java
@@ -0,0 +1,11 @@
+package cz.jacktech.dbr.library;
+
+/**
+ * Created by toor on 20.7.14.
+ */
+public interface ReportingService {
+
+    public void report(String title, String text);
+    public void auth(String username, String password);
+
+}
---
+++ library/src/main/java/cz/jacktech/dbr/library/services/RedmineService.java
@@ -0,0 +1,18 @@
+package cz.jacktech.dbr.library.services;
+
+import cz.jacktech.dbr.library.ReportingService;
+
+/**
+ * Created by toor on 20.7.14.
+ */
+public class RedmineService implements ReportingService {
+    @Override
+    public void report(String title, String text) {
+
+    }
+
+    @Override
+    public void auth(String username, String password) {
+
+    }
+}
